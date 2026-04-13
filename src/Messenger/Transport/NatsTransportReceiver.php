f61a42ae3b0e46bb6fc216f9d73525717e5b9805
Jesse Jeronimus <jesse@peterprint.nl>
2026-04-13 16:00:20 +0200

    DEV-4603 - added BackedEnumNormalizer

--- src/Messenger/Transport/NatsTransportReceiver.php
+++ src/Messenger/Transport/NatsTransportReceiver.php
@@ -24,6 +24,7 @@ use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
@@ -153,6 +154,7 @@ class NatsTransportReceiver implements ReceiverInterface
             );
 
             $this->denormalizer = new Serializer([
+                new BackedEnumNormalizer(),
                 new ArrayDenormalizer(),
                 new ObjectNormalizer(propertyTypeExtractor: $propertyTypeExtractor),
             ]);
